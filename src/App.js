e388529aeebc8947bef6026d204b58adddcf541a
Jake Garlick <jacobgarlick@msn.com>
2019-11-01 18:12:39 -0600

    began to build out logic for game to work based on click events, built out basic rendering for game

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import imageCards from "./components/imageCards";
+// import logo from './logo.svg';
+import Wrapper from "./components/wrapper"
+import ImageCards from "./components/imageCards";
 import images from "./images.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  state = {
+    images,
+    count: 0
+  };
+
+  userScore = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  userReset = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h1 className="title">Clicky Game - React</h1>
+        <h2 className="userScore">Your Score: {this.state.count}</h2>
+        {this.state.images.map(gamePlay => (
+        <ImageCards
+          key={gamePlay.id}
+          id={gamePlay.id}
+          name={gamePlay.name}
+          image={gamePlay.image}
+          occupation={gamePlay.occupation}
+          location={gamePlay.location}
+        />
+        ))}
+      </Wrapper>
+    )
+  }
+};
 
 export default App;
+
+// handleIncrement = () => {
+//   this.setState({ count: this.state.count + 1 });
+// };
+
+// render() {
+//   return (
+//     <div className="card text-center">
+//       <div className="card-header bg-primary text-white">
+//         Click Counter!
+//       </div>
+//       <div className="card-body">
+//         <p className="card-text">Click Count: {this.state.count}</p>
+//         <button className="btn btn-primary" onClick={this.handleIncrement}>
+//           Increment
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
--- src/components/imageCards/index.js
+++ src/components/imageCards/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import "./style.css";
 
-function ImageCards(props) {
+class ImageCards extends React.Component {
+
+  state = {
+    count: 0
+  };
+
+  userScore = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  userReset = () => {
+    this.setState({ count: 0 });
+  };
+
   return (
     <div className="card">
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
       <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-        </ul>
+            <strong>Click Me!</strong>
+            <button className="btn btn-primary" onClick={this.userScore}>Test</button>
       </div>
     </div>
-  );
-}
+  )
+};
 
 export default ImageCards;
